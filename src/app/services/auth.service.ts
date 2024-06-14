f8e11603e60cd5f5836586658ecd2e23936b81d6
davidmg512 <davidmgaspe@gmail.com>
2024-06-14 11:25:09 +0200

    cambios2

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,18 +8,26 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   private readonly USER_KEY = 'user';
+  private secretKey = 'mi-clave-secreta';
 
   constructor() { }
 
   register(user: any): void {
-    localStorage.setItem(this.USER_KEY, JSON.stringify(user));
+    const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(user), this.secretKey).toString();
+    //localStorage.setItem(this.USER_KEY, JSON.stringify(user));
+    localStorage.setItem(this.USER_KEY, encryptedData);
+  }
+
+  decryptData(encryptedData: string): any {
+    const bytes = CryptoJS.AES.decrypt(encryptedData, this.secretKey);
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
   }
 
   login(correo: string, password: string): boolean {
     const storedUser = localStorage.getItem(this.USER_KEY);
     
     if (storedUser) {
-      const user = JSON.parse(storedUser);
+      const user = this.decryptData(storedUser);
       if (user.correo === correo && user.password === password) {
         return true;
       }
